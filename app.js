35e51bc3324ecf4f54ea56fc203609ec4fd50062
Vikas5686 <91067260+Vikas5686@users.noreply.github.com>
2023-10-17 18:03:05 +0530

    basic setup done

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("this is my server");
+const express = require('express');
+require("dotenv").config();
+const app = express()
+const cors = require("cors")
+const { mongoose } = require('mongoose');
+
+require("./db/connection")
+// const users = require("./models/UserShema")
+const router =require("./routes/route")
+
+const port = 3000;
+
+app.use(cors())
+app.use(express.json())
+app.use(router)
+
+app.listen(port, () => {
+    console.log("Server start ")
+})
---
+++ routes/route.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const router = express.Router()
+const users = require("../models/UserShema")
+
+
+
+router.post("/register", async (req, res) => {
+    const { name, country, email, Score } = req.body
+    try {
+        if (!name || !country || !email) {
+            res.status(403).send("plz fill the form")
+        } else {
+            const preuser = await users.findOne({ email: email })
+            console.log(preuser)
+            if (preuser) {
+                res.status(404).send("already exist")
+            }
+            else {
+                const AddNewUser = new users({
+                    name, country, email, Score
+                })
+                await AddNewUser.save()
+                console.log(AddNewUser._id)
+                
+                res.status(201).json(AddNewUser._id)
+            }
+        }
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+
+router.get("/getrequist", async (req, res) => {
+    try {
+        const user = await users.find().sort({ "Score": -1 });
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+router.get("/delete", async (req, res) => {
+    try {
+        const user = await users.deleteMany();
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+
+router.get("/getUser/:id", async (req, res) => {
+    try {
+        console.log(req.params)
+        const { id } = req.params
+        const userindividual = await users.find({ email: id })
+        console.log(userindividual)
+        res.status(201).json(userindividual)
+    } catch (error) {
+        console.log("djflsfj")
+        res.status(404).json(error)
+    }
+})
+router.patch("/Update/:id", async (req, res) => {
+    try {
+        console.log(req.body)
+        const { id } = req.params
+        const userindividual = await users.findOneAndUpdate({ _id: id }, { $set: { Score: req.body.Score } })
+        if (userindividual==null) {
+            console.log(userindividual)
+        }
+        res.status(201).json(userindividual)
+    } catch (error) {
+        res.status(404).json(error)
+    }
+})
+
+
+module.exports = router;
